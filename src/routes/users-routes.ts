d264f194e6505833c82a61400845058ca9fb2146
danmakowiesky <dvm1993@hotmail.com>
2023-10-14 09:40:17 -0300

    cadastro de usuario

--- src/routes/users-routes.ts
+++ src/routes/users-routes.ts
@@ -1,8 +1,22 @@
 import { FastifyInstance } from 'fastify'
-
+import {knex} from '../database'
+import {z} from 'zod'
+import { randomUUID } from 'node:crypto'
 export async function usersRoutes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
     console.log('hello')
     return reply.status(200).send({message: 'usuario hello'})
+  })
+
+  app.post('/', async (request, reply) => {
+    const createUsersBody = z.object({
+      name: z.string()
+    })
+    const {name} = createUsersBody.parse(request.body)
+    await knex('users').insert({
+      id: randomUUID(),
+      name
+    })
+    reply.status(201).send()
   })
 }
